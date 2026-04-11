f1973c0cd0457c6a7ac3500cc53d277cdbc9d61b
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-04-11 16:31:45 +0800

    更新 LspHandler.kt

--- core/app/src/main/java/com/itsaky/androidide/handlers/LspHandler.kt
+++ core/app/src/main/java/com/itsaky/androidide/handlers/LspHandler.kt
@@ -21,9 +21,10 @@ import android.content.Context
 import com.itsaky.androidide.lsp.api.ILanguageClient
 import com.itsaky.androidide.lsp.api.ILanguageServerRegistry
 import com.itsaky.androidide.lsp.java.JavaLanguageServer
-import com.itsaky.androidide.lsp.kotlin.KotlinLanguageServer
+// import com.itsaky.androidide.lsp.kotlin.KotlinLanguageServer
 import com.itsaky.androidide.lsp.servers.toml.TomlServer
 import com.itsaky.androidide.lsp.xml.XMLLanguageServer
+import com.itsaky.androidide.lsp.kotlin.lsp.KotlinLspServer
 
 /** @author Akash Yadav */
 object LspHandler {
@@ -32,7 +33,7 @@ object LspHandler {
     ILanguageServerRegistry.getDefault().apply {
       getServer(JavaLanguageServer.SERVER_ID) ?: register(JavaLanguageServer())
       getServer(XMLLanguageServer.SERVER_ID) ?: register(XMLLanguageServer())
-      getServer(KotlinLanguageServer.SERVER_ID) ?: register(KotlinLanguageServer(context))
+      getServer(KotlinLspServer.SERVER_ID) ?: register(KotlinLspServer(context))
       getServer(TomlServer.SERVER_ID) ?: register(TomlServer())
     }
   }
