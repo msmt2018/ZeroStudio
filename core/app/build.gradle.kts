54b80093a6bffdbb8ecfbba629951eab82a12713
android-zeros <16684848382@163.com>
2026-04-27 22:09:23 +0800

    add zero-Symbol-input-view

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -242,7 +242,7 @@ dependencies {
   implementation(projects.event.eventbusEvents)
   implementation(projects.modules.mtDataFilesProvider)
   implementation(projects.modules.deviceCompat)
-  implementation(projects.zeroSymbolInputView)
+  implementation(projects.modules.zeroSymbolInputView)
   implementation(projects.core.git)
   implementation(projects.core.layoutEditor)
   // implementation(projects.core.chatai.app)
