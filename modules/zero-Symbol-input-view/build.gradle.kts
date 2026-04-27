249f84bf025a8556cad7dfd4d59b07b08b951c28
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-04-27 20:52:03 +0800

    更新 build.gradle.kts

--- modules/zero-Symbol-input-view/build.gradle.kts
+++ modules/zero-Symbol-input-view/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.library")
-    id("com.vanniktech.maven.publish.base")
 }
 
 android {
@@ -8,15 +7,14 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation(libs.google.material)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.gson)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.viewpager)
+    implementation(libs.androidx.recyclerview)
 
-    implementation(projects.editor)
+    implementation(libs.common.editor)
     api(libs.androidx.annotation)
-    implementation(projects.languageJava)
-    implementation(projects.languageTextmate)
 
 }
