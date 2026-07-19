10e04b3576c9d6fc4266889b7dad298baff410d7
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-07-19 14:04:23 +0800

    fix(java): AdbChannel interface should use 'extends' not 'implements'
    In Java, an interface inheriting from another interface must use 'extends'.
    'implements' is only for classes implementing interfaces.
    
    Error:
      AdbChannel.java:9: error: '{' expected
      public interface AdbChannel implements Closeable {
                                 ^
    
    Fix:
      public interface AdbChannel extends Closeable {

--- debugger/adb-connection/adblib/src/main/java/android/zero/studio/adblib/AdbChannel.java
+++ debugger/adb-connection/adblib/src/main/java/android/zero/studio/adblib/AdbChannel.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 /**
  * Created by xudong on 2/21/14.
  */
-public interface AdbChannel implements Closeable {
+public interface AdbChannel extends Closeable {
     void readx(byte[] buffer, int length) throws IOException;
 
     void writex(AdbMessage message) throws IOException;
